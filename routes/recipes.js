5db071c523ed823539cd651af36143f8d7ea9d6e
ShahafErez <Shahaf30@gmail.com>
2022-05-28 20:34:43 +0300

    get random recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,16 @@ const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
+router.get("/random", async (req, res, next) => {
+  let num_of_recipes = 3;
+  try {
+    let random_recipes = await recipes_utils.getRandomRecipies(num_of_recipes);
+    res.send(random_recipes);
+  } catch (error) {
+    next(error);
+  }
+});
+
 /**
  * This path returns a full details of a recipe by its id
  */
