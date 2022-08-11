48415b4bda330cebec85e85fb5566db551da275e
ChenMordehai <81912418+ChenMordehai@users.noreply.github.com>
2022-08-11 20:05:48 +0300

    status

--- routes/auth.js
+++ routes/auth.js
@@ -67,7 +67,7 @@ router.post("/Login", async (req, res, next) => {
 
 router.post("/Logout", function (req, res) {
   req.session.reset(); // reset the session info --> send cookie when  req.session == undefined!!
-  res.send({ success: true, message: "logout succeeded" });
+  res.statut(200).send({ success: true, message: "logout succeeded" });
 });
 
 module.exports = router;
